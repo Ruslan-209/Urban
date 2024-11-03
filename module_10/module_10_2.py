8ff896d5f94149b34cfba9a55a38602f45e5eb47
Ruslan <itachi_7@mail.ru>
2024-11-03 22:12:57 +0300

    Add files via upload

--- module_10/module_10_2.py
+++ module_10/module_10_2.py
@@ -48,10 +48,10 @@ import time
 
 
 class Knight(threading.Thread):
-    def __init__(self, name, power):
-        threading.Thread.__init__(self)
-        self.name = str(name)
-        self.power = int(power)
+    def __init__(self, name: str, power: int):
+        super().__init__()
+        self.name = name
+        self.power = power
 
     def run(self):
         health_points = 100
@@ -61,11 +61,14 @@ class Knight(threading.Thread):
             health_points -= self.power
             days += 1
             time.sleep(1)
+            if health_points < 0:
+                break
             print(f"{self.name} сражается {days} дней(дня), осталось {health_points} воинов.")
+
         print(f"{self.name} одержал победу спустя {days} дней(дня)!")
 
 
-first_knight = Knight('Sir Lancelot', 10)
+first_knight = Knight('Sir Lancelot', 11)
 second_knight = Knight("Sir Galahad", 20)
 
 first_knight.start()
@@ -75,7 +78,3 @@ first_knight.join()
 second_knight.join()
 
 print('Все битвы закончились!')
-
-
-
-
